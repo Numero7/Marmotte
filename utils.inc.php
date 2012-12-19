3279ab58c4e5c643da9b470cba3a2e60c4880f27
yannponty <yann.ponty@lix.polytechnique.fr>
2012-12-19 11:50:45 +0100

    Nouvelle fonctions de filtrage et présentation rapide des résultats

--- utils.inc.php
+++ utils.inc.php
@@ -173,12 +173,103 @@
 		global $fieldsSummary;
 		global $fieldsAll;
 		global $actions;
+		global $typesEval;
 		
 		$result = filterSortReports($id_session, $type_eval, $sort_crit, $login_rapp);
 		$sortCrit = parseSortCriteria($sort_crit);
-		
+		$rapporteurs = array();
+		$sessions =  showSessions();
+		$rows = array();
+		while ($row = mysql_fetch_object($result))
+		{
+			$rows[] = $row;
+			$rapporteurs[$row->rapporteur] = 1;
+		}
+		$krapp = array_keys($rapporteurs);
+		natcasesort($krapp);
+		$rapporteurs = $krapp;
 
 		?>
+			<h2>Filtrage</h2>
+			<form  method="get">
+				<table class="inputreport">
+					<tr>
+						<td style="width:20em;">Session</td>
+						<td style="width:20em;"><select name="id_session"> 
+						<option value="-1">Toutes les sessions</option>
+						<?php
+							foreach ($sessions as $val)
+							{
+								$sel = "";
+								if ($val["id"]==$id_session)
+								{ $sel = " selected=\"selected\""; }
+								echo "<option value=\"".$val["id"]."\" $sel>".ucfirst($val["nom"])." ".date("Y",strtotime($val["date"]))."</option>";
+							}
+						?>
+						</select></td>
+						<td>
+							<input type="hidden" name="login_rapp" value="<?php echo $login_rapp;?>">
+							<input type="hidden" name="type_eval" value="<?php echo $type_eval;?>">
+							<input type="hidden" name="sor_crit" value="<?php echo $sort_crit;?>">
+							<input type="hidden" name="action" value="view">
+							<input type="submit" value="Filtrer">
+						</td>
+					</tr>
+				</table>
+			</form>
+			<form  method="get">
+				<table class="inputreport">
+					<tr>
+						<td style="width:20em;">Rapporteur</td>
+						<td style="width:20em;"><select name="login_rapp"> 
+						<option value="">Tous les rapporteurs</option>
+						<?php
+							foreach ($rapporteurs as $rapp)
+							{
+								$sel = "";
+								if ($rapp==$login_rapp)
+								{ $sel = " selected=\"selected\""; }
+								echo "<option value=\"$rapp\"$sel>".ucfirst($rapp)."</option>";
+							}
+						?>
+						</select></td>
+						<td>
+							<input type="hidden" name="id_session" value="<?php echo $id_session;?>">
+							<input type="hidden" name="type_eval" value="<?php echo $type_eval;?>">
+							<input type="hidden" name="sor_crit" value="<?php echo $sort_crit;?>">
+							<input type="hidden" name="action" value="view">
+							<input type="submit" value="Filtrer">
+						</td>
+					</tr>
+				</table>
+			</form>
+			<form  method="get">
+				<table class="inputreport">
+					<tr>
+						<td style="width:20em;">Type évaluation</td>
+						<td style="width:20em;"><select name="type_eval"> 
+						<option value="">Tous les types</option>
+						<?php
+							foreach ($typesEval as $ty)
+							{
+								$sel = "";
+								if ($ty==$type_eval)
+								{ $sel = " selected=\"selected\""; }
+								echo "<option value=\"$ty\"$sel>".ucfirst($ty)."</option>";
+							}
+						?>
+						</select></td>
+						<td>
+							<input type="hidden" name="id_session" value="<?php echo $id_session;?>">
+							<input type="hidden" name="sor_crit" value="<?php echo $sort_crit;?>">
+							<input type="hidden" name="action" value="view">
+							<input type="hidden" name="login_rapp" value="<?php echo $login_rapp;?>">
+							<input type="submit" value="Filtrer">
+						</td>
+					</tr>
+				</table>
+			</form>
+			<br><hr><br>
 			<h2>Rapports disponibles</h2>
 			<table class="summary">
 				<tr>
@@ -198,10 +289,8 @@
 		?>
 				</tr>
 		<?php
-		$rapporteurs = array();
-		while ($row = mysql_fetch_object($result))
+		foreach($rows as $row)
 		{
-			$rapporteurs[$row->rapporteur] = 1;
 		?>
 				<tr>
 			<?php
@@ -220,35 +309,8 @@
 				<tr>
 		<?php
 		}
-		$krapp = array_keys($rapporteurs);
-		natcasesort($krapp);
-		$rapporteurs = $krapp;
 		?>
 			</table>
-			<br><hr><br>
-			<form  method="get">
-				<table class="inputreport">
-					<tr>
-						<td style="width:20em;">Rapporteur</td>
-						<td style="width:20em;"><select name="login_rapp"> 
-						<option value="">Tous les rapporteurs</option>
-						<?php
-							foreach ($rapporteurs as $rapp)
-							{
-								echo "<option value=\"$rapp\">".ucfirst($rapp)."</option>";
-							}
-						?>
-						</select></td>
-						<td>
-							<input type="hidden" name="id_session" value="<?php echo $id_session;?>">
-							<input type="hidden" name="type_eval" value="<?php echo $type_eval;?>">
-							<input type="hidden" name="sor_crit" value="<?php echo $sort_crit;?>">
-							<input type="hidden" name="action" value="view">
-							<input type="submit" value="Filtrer rapporteurs">
-						</td>
-					</tr>
-				</table>
-			</form>
 		<?php
 	} ;	
 
