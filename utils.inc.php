6e23396e1bee5c5cedc4df5a9441c6850cf1ee47
Numero7 <hugo.gimbert@gmail.com>
2013-09-20 12:58:58 +0200

    email verbosity

--- utils.inc.php
+++ utils.inc.php
@@ -323,12 +323,18 @@ function message_handler($subject,$body)
 
 function email_handler($recipient,$subject,$body)
 {
+	echo "Trying to send email to '".$recipient."' with subject '".$subject."'... ";
+	
 	$headers = 'From: '.get_config("webmaster"). "\r\n" . 'CC: ' .get_config("webmaster"). "\r\n". 'Reply-To: '.get_config("webmaster"). "\r\n".'Content-Type: text/plain; charset="UTF-8"\r\n'.'X-Mailer: PHP/' . phpversion()."\r\n";
 
 	$result = mail($recipient, $subject, "\r\n".$body."\r\n", $headers);
 
 	if($result == false)
+	{
+		echo "failed!";
 		throw new Exception("Could not send email to ".$recipient." with subject ".$subject);
+	}
+		echo "sucess.";
 }
 
 
