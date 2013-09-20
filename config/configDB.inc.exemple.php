d2937d4cd1a056a131b908ecce89c26e8d6c1cb3
Numero7 <hugo.gimbert@gmail.com>
2013-09-20 13:22:18 +0200

    added DB exemple

---
+++ config/configDB.inc.exemple.php
@@ -0,0 +1,17 @@
+<?php
+	
+	$servername = "127.0.0.1";
+	$dbname = "cn6";
+	$serverlogin = "monlogin";
+	$serverpassword = "monmotdepasse";
+	
+	$rootdir = "";
+	$tmpdir = "C:/phptmp/";
+	
+	define("reports_db","reports");
+	define("users_db","users");
+	define("sessions_db","sessions");
+	define("units_db","units");
+	define("people_db","people");
+	
+?>
