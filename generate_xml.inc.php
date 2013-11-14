4df60119fc99eaac93e4c78adbb3598243f6bce6
Numero7 <hugo.gimbert@gmail.com>
2013-11-14 16:58:28 +0100

    correction accents dans les formules standards

--- generate_xml.inc.php
+++ generate_xml.inc.php
@@ -297,8 +297,10 @@ function createXMLReportElem($row, DOMDocument $doc, $keep_br = true)
 		{
 			$formula = $formulas[$row->avis];
 //			appendLeaf("formulestandard",  $keep_br ? $row->$fieldID : remove_br($formula) , $doc, $rapportElem);
-			$row->rapport .= "<br/><br/>".normalizeField($formula);
-		}
+			$row->rapport .= "<br/><br/>".stripInvalidXml($formula);//normalizeField($formula);
+//			rrr();
+			
+		}		
 	}
 
 	global $fieldsRapportAll;
