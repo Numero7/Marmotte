6a6d256e37fe7f7ac2b35b46e045ce4ff318aeff
Numero7 <hugo.gimbert@gmail.com>
2014-11-09 16:40:31 +0100

    bug

--- display_field.inc.php
+++ display_field.inc.php
@@ -242,6 +242,7 @@ function display_fichiers($row, $fieldID, $session, $readonly)
 	}
 	else
 	{
+		if(isset($row->unite) && $row->unite == "") return;
 		$files = find_people_files($row,true, $session, true);
 		$dir = get_people_directory($row, $session, false);
 	}
