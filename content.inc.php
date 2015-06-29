99e25af8121b6142cd82e0d930bf915438e358c8
Numero7 <hugo.gimbert@labri.fr>
2015-06-29 11:15:46 +0200

    affichage

--- content.inc.php
+++ content.inc.php
@@ -3,11 +3,17 @@
 
 require_once("header.inc.php");
 require_once("authbar.inc.php");
+echo "<!-- 1 -->";
 require_once('display_report.inc.php');
+echo "<!-- 1 -->";
 require_once('display_reports.inc.php');
+echo "<!-- 1 -->";
 require_once('manage_filters_and_sort.inc.php');
+echo "<!-- 1 -->";
 require_once('manage_concours.inc.php');
+echo "<!-- 1 -->";
 require_once('export.inc.php');
+echo "<!-- 1 -->";
 
 $id = current_session_id();
 
@@ -675,3 +681,7 @@ function alertText($text)
 		?>
 	</div>
 </div>
+<br/>
+<br/>
+<br/>
+<br/>
